71f808b8827a27c2fc4b2804ebd106826723d41f
unknown <bassey.rhema@yahoo.com>
2024-10-01 12:54:53 +0100

    addcheckboxes and outlines once clicked

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+var n = 0
 document
   .getElementById("inputSection")
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevents the default form submission
-    task = document.getElementById("task").value;
+    task = document.getElementById("task");
 
     const entry = document.getElementById("taskList");
-    entry.innerHTML += `<ul><li>${task}</li></ul>`;
+    entry.innerHTML += `<div><ul><li id="${n}"><input onclick="toggleCrossOut(${n})" type="checkbox">${task.value}</input></li></ul></div>`;
+    task.value = ''
 
+    n++
+    console.log(n)
 
-  
   });
+  function toggleCrossOut(n) {
+    select_task = document.getElementById(n)
+    select_task.classList.toggle('crossed-out'); // Toggles the 'crossed-out' class
+}
