b67e2442d857196bbbd195bb3bfeb19dd03f02c4
unknown <bassey.rhema@yahoo.com>
2024-10-06 21:47:29 +0100

    add functioning delete button

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-var index = 0 //asigns index to list items
+var index = 0; //asigns index to list items
+const fullListID = "taskList";
+const entry = document.getElementById(fullListID);
+const deleteBtn = document.getElementById("deleteBtn");
+
+//takes note of crossed out items
+function crossedItems() {
+  return entry.querySelectorAll(".crossed-out");
+}
+
 document
   .getElementById("inputSection")
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevents the default form submission
     task = document.getElementById("task");
 
-    const entry = document.getElementById("taskList");
-    if (task.value != ''){
+    if (task.value != "") {
       entry.innerHTML += `<div><ul><li id="${index}"><input onclick="toggleCrossOut(${index})" type="checkbox">${task.value}</input></li></ul></div>`;
     }
 
-    task.value = ''
-    index++
+    task.value = "";
+    index++;
 
-    crossedItems = entry.querySelectorAll('.crossed-out');
     // persist the checkboxes after submit button is pressed
-    crossedItems.forEach(function(item){
-      item.querySelector('input').checked = true
-    })
-
+    crossedItems().forEach(function (item) {
+      item.querySelector("input").checked = true;
+    });
   });
 
-  // assigns crossed-out text-decoration to list if input box is pressed
-  function toggleCrossOut(n) {
-    select_task = document.getElementById(n)
-    select_task.classList.toggle('crossed-out'); // Toggles the 'crossed-out' class
+// assigns crossed-out text-decoration to list if input box is pressed
+
+function toggleCrossOut(n) {
+  var select_task = document.getElementById(n);
+  select_task.classList.toggle("crossed-out"); // Toggles the 'crossed-out' class
+
+
+
+  // this toggles the hide class, only if you go from 0 to 1 or 1 to 0
+  if(crossedItems().length > 0){
+    deleteBtn.classList.remove("hide");
+  } else{
+    deleteBtn.classList.add("hide");
+  }
+
 }
 
-
+function deleteItems(){
+  console.log("deleted")
+  crossedItems().forEach(function (item) {
+    item.remove()
+    deleteBtn.classList.add("hide");
+  });
+}
