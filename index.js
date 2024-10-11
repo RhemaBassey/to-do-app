942ea204316591587674e698fc53c7ecb917bacc
unknown <bassey.rhema@yahoo.com>
2024-10-11 14:46:41 +0100

    add edit functionality and enable canceling edit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function crossedItems() {
   return entry.querySelectorAll(".crossed-out");
 }
 
+function editIcon(index) {
+  return `<span class="hide"><i class="fas fa-edit" onclick="editMode(${index})"></i></span>`;
+}
+
 document
   .getElementById("inputSection")
   .addEventListener("submit", function (event) {
@@ -15,11 +19,33 @@ document
     task = document.getElementById("task");
 
     if (task.value != "") {
-      entry.innerHTML += `<div><ul><li id="${index}"><input onclick="toggleCrossOut(${index})" type="checkbox">${task.value}</input></li></ul></div>`;
+      entry.innerHTML += `<li id="${index}"><input 
+      onclick="toggleCrossOut(${index})" type="checkbox">${
+        task.value
+      }${editIcon(index)}</li>`;
     }
-
     task.value = "";
     index++;
+
+    entryItems = document.querySelectorAll("li");
+
+    entryItems.forEach((entryItem) => {
+      // Add event listener for mouseover (hover)
+      entryItem.addEventListener("mouseover", () => {
+        try {
+          icon = entryItem.querySelector("span");
+          icon.classList.remove("hide");
+        } catch (error) {}
+      });
+
+      // Add event listener for mouseout (when mouse leaves)
+      entryItem.addEventListener("mouseout", () => {
+        try {
+          icon = entryItem.querySelector("span");
+          icon.classList.add("hide");
+        } catch (error) {}
+      });
+    });
 
     // persist the checkboxes after submit button is pressed
     crossedItems().forEach(function (item) {
@@ -27,27 +53,50 @@ document
     });
   });
 
-// assigns crossed-out text-decoration to list if input box is pressed
+function edit(index, listItemText) {
+  const listItem = document.getElementById(index);
+  checkBoxState = listItem.querySelector("input").checked
 
+  listItem.innerHTML = `<input 
+      onclick="toggleCrossOut(${index})" type="checkbox">${listItemText}${editIcon(
+    index
+  )}`;
+
+    // persist the checkboxes after submit button is pressed
+    listItem.querySelector("input").checked = checkBoxState
+}
+
+
+function editMode(index) {
+  const listItem = document.getElementById(index);
+  checkBoxState = listItem.querySelector("input").checked
+
+
+  var editLayout = `<br><textarea>${listItem.innerText}
+      </textarea><button>Ok</button><button onclick="edit(${index},'${listItem.innerText}')">Cancel</button>`;
+  listItem.innerHTML = `<input 
+      onclick="toggleCrossOut(${index})" type="checkbox">${editLayout}</li>`;
+
+  // persist the checkboxes after submit button is pressed
+  listItem.querySelector("input").checked = checkBoxState
+}
+
+// assigns crossed-out text-decoration to list if input box is pressed
 function toggleCrossOut(n) {
   var select_task = document.getElementById(n);
   select_task.classList.toggle("crossed-out"); // Toggles the 'crossed-out' class
 
-
-
   // this toggles the hide class, only if you go from 0 to 1 or 1 to 0
-  if(crossedItems().length > 0){
+  if (crossedItems().length > 0) {
     deleteBtn.classList.remove("hide");
-  } else{
+  } else {
     deleteBtn.classList.add("hide");
   }
-
 }
 
-function deleteItems(){
-  console.log("deleted")
+function deleteItems() {
   crossedItems().forEach(function (item) {
-    item.remove()
+    item.remove();
     deleteBtn.classList.add("hide");
   });
 }
